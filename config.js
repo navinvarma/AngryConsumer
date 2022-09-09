d53253db63a956197c1ac8c365b4eff4829095ab
Navin Varma <navinvarma@gmail.com>
2022-09-08 17:41:32 -0700

    Remove unused var

--- config.js
+++ config.js
@@ -1,7 +1,6 @@
 module.exports = {
-	'secret': '}!&G~g?0SEOj6c4Ph91jd9f+!59^UA',
 	'node_port': 3000,
 	'mongo_host': 'localhost',
 	'mongo_port': 27017,
-	'db_name' : 'consumer-finance-explore'
+	'db_name': 'consumer-finance-explore'
 };
